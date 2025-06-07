208c6e71dcf3b6cdb666a4e5dd9bf1615d8a9f7a
Héloise Akohou <ak.heloise2007@gmail.com>
2025-06-07 23:32:48 +0200

    configuration de base et mise en place du modele User

---
+++ gotogether/authentication/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    #Liste des choix
+    ROLES_CHOICES = [
+        ('conducteur', 'Conducteur'),
+        ('passager', 'Passager'),
+    ]
+    email = models.EmailField(unique= True, verbose_name= ' Adresse e-mail')
+
+
+    numero_telephone = models.CharField(
+        max_length=15,
+        unique=True,
+        verbose_name='Numéro de téléphone'
+    )
+
+
+    photo_profil = models.ImageField(
+        upload_to='photos_profil/', # Toutes les photos de profil seront enregistrées dans le dossier 'photos_profil' a l'intérieur du dossier 'media'
+        null=True,
+        blank=True,
+        verbose_name='Photo de profil'
+    )
+
+
+    heure_depart = models.TimeField(
+        null=True,
+        blank=True,
+        verbose_name='Heure de départ'
+    )
+
+
+   # champ pour l'emplacement de l'utilisateur
+
+    heure_arrivee = models.TimeField(
+        null=True,
+        blank=True,
+        verbose_name='Heure d\'arrivée'
+    )
+    #Champ pour le role de l'utilisateur
+    role = models.CharField(
+        max_length=20,
+        choices=ROLES_CHOICES,
+        verbose_name='Rôle',
+        default='passager'  # Valeur par défaut
+    )
