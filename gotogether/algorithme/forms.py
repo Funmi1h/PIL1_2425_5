8c41a608dba9f7a8acbfe3d4c94f592736be5a26
FreudArthur <bokossafreud6@gmail.com>
2025-06-13 14:17:49 +0200

    Achèvement du formulaire

--- gotogether/algorithme/forms.py
+++ gotogether/algorithme/forms.py
@@ -7,22 +7,30 @@ conducteur = User.objects.filter(role='conducteur')
 
 class ConducteurForm(forms.ModelForm):
     class Meta:
-        model = Conducteur
-        fields = ['latitude_conducteur', 'longitude_conducteur', 'nb_places', 'adresse']
-        widgets = {
-            'latitude_conducteur': forms.NumberInput(attrs={'step': 'any'}),
-            'longitude_conducteur': forms.NumberInput(attrs={'step': 'any'}),
-            'nb_places': forms.NumberInput(attrs={'min': 1, 'max': 5}),
-            'adresse': forms.TextInput(attrs={'placeholder': 'Indiquez votre adresse'}),
-        }
+        model = conducteur
+        adresse = forms.CharField(
+            max_length= 100,
+            label= 'Entrez votre adresse' , 
+            widget= forms.TextInput(attrs={'placeholder':'Adresse ou repère'})
+        )
+        
+        nb_places = forms.IntegerField(
+            label= 'nombre de places du véhicule', 
+            widget= forms.TextInput(attrs={'placeholder':'nombre de places'})
+        ) 
+
+        
+        
 
 
 class ClientForm(forms.ModelForm):
     class Meta:
-        model = Client
-        fields = ['latitude_client', 'longitude_client', 'adresse']
-        widgets = {
-            'latitude_client': forms.NumberInput(attrs={'step': 'any'}),
-            'longitude_client': forms.NumberInput(attrs={'step': 'any'}),
-            'adresse': forms.TextInput(attrs={'placeholder': 'Indiquez votre adresse'}),
-        }
+        model = client
+
+        adresse = forms.CharField(
+            max_length= 100,
+            label= 'Entrez votre adresse' , 
+            widget= forms.TextInput(attrs={'placeholder':'Adresse ou repère'})
+        ) 
+
+        
