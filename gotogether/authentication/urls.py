b33aede2465224faa75e72c79a26183e97c2700e
Héloise Akohou <ak.heloise2007@gmail.com>
2025-06-13 17:20:47 +0200

    Logique de changement de mot passe et mise a jour de photo de profile

--- gotogether/authentication/urls.py
+++ gotogether/authentication/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('profil/', views.profil_user, name= 'profil_user'),
     path('dashboard/', views.dashboard, name = 'dashboard'),
     path('logout/', views.logout_user,name ='logout'),
+    path('auth/change_user_password', views.upload_password, name= 'upload_password'),
+    path('auth/change_user_profile_photo',  views.upload_profile_photo, name= 'update_photo_profil')
     
 ]
 
