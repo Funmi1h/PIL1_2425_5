1dd9d83731a42d6621791495701d3cbafbf87f35
Héloise Akohou <ak.heloise2007@gmail.com>
2025-06-17 21:08:45 +0200

    Correction de la réinitialisation du mot de passe

--- gotogether/authentication/urls.py
+++ gotogether/authentication/urls.py
@@ -12,19 +12,19 @@ urlpatterns = [
     path('change_user_profile_photo',  views.upload_profile_photo, name= 'update_photo_profil'),
     path('update_profile/', views.modifier_profil, name= 'changer_profil' ),
      path('mot-de-passe-oublie/', auth_views.PasswordResetView.as_view(
-        template_name='auth/password_reset_form.html'
+        template_name='authentication/password_reset_form.html'
     ), name='password_reset'),
 
     path('mot-de-passe-envoye/', auth_views.PasswordResetDoneView.as_view(
-        template_name='auth/password_reset_done.html'
+        template_name='authentication/password_reset_done.html'
     ), name='password_reset_done'),
 
     path('reinitialisation/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='auth/password_reset_confirm.html'
+        template_name='authentication/password_reset_confirm.html'
     ), name='password_reset_confirm'),
 
     path('mot-de-passe-termine/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='auth/password_reset_complete.html'
+        template_name='authentication/password_reset_complete.html'
     ), name='password_reset_complete'),
 
     path('geocode_proxy/', views.geocode_proxy, name='geocode_proxy'),
